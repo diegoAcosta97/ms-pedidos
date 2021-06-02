8d11490ce146c0873aa81b5fc3478549d1c34119
Diego Acosta <dacosta@frsf.utn.edu.ar>
2021-06-02 18:58:44 -0300

    Commit inicial

---
+++ src/test/java/danapt/serviciopedidos/ServicioPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package danapt.serviciopedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
